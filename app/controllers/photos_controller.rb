5ee38089f1eab7c160c75d93908693b10d083a6a
kjplank <plank@dhcp-10-105-169-209.wireless.northwestern.private>
2015-05-07 08:42:19 -0500

    Completed create and delete portions

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -7,4 +7,23 @@ class PhotosController < ApplicationController
     @photo = Photo.find(params["id"])
   end
 
+  def new_form
+  end
+
+  def create_row
+    p = Photo.new
+    p.source = params[:the_source]
+    p.caption = params[:the_caption]
+    p.save
+
+    redirect_to("http://localhost:3000/photos")
+  end
+
+  def destroy
+   p = Photo.find(params[:id])
+   p.destroy
+
+   redirect_to("http://localhost:3000/photos")
+  end
+
 end
